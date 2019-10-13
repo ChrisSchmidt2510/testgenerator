ee558e306c464693ccc7f1b9fa373047bea7d883
Schmidt, Christoph <Christoph.Schmidt@nuernberger.de>
2019-10-13 18:22:08 +0200

    Bugfix FieldTypeChanger
    Verbesserung des Loggings

--- testgenerator-core/src/main/java/de/nvg/testgenerator/logging/Logger.java
+++ testgenerator-core/src/main/java/de/nvg/testgenerator/logging/Logger.java
@@ -1,161 +1,142 @@
 package de.nvg.testgenerator.logging;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.function.Supplier;
 
-public class Logger
-{
-  private static final Logger INSTANCE = new Logger();
+public class Logger {
+	private static final Logger INSTANCE = new Logger();
 
-  private Level level;
+	private Level level;
 
-  private String className;
-  private String methodName;
-  private String methodDescriptor;
+	private String className;
+	private String methodName;
+	private String methodDescriptor;
 
-  private Logger()
-  {}
+	private Logger() {
+	}
 
-  public void setClassName(String className)
-  {
-    this.className = className;
-  }
+	public void setClassName(String className) {
+		this.className = className;
+	}
 
-  public void setMethodName(String methodName)
-  {
-    this.methodName = methodName;
-  }
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
 
-  public void setMethodDescriptor(String methodDescriptor)
-  {
-    this.methodDescriptor = methodDescriptor;
-  }
+	public void setMethodDescriptor(String methodDescriptor) {
+		this.methodDescriptor = methodDescriptor;
+	}
 
-  public void setLevel(Level level)
-  {
-    this.level = level;
-  }
+	public void resetMethod() {
+		this.methodName = null;
+		this.methodDescriptor = null;
+	}
 
-  public static Logger getInstance()
-  {
-    return INSTANCE;
-  }
+	public void setLevel(Level level) {
+		this.level = level;
+	}
 
-  public void error(String message)
-  {
-    log(Level.ERROR, message);
-  }
+	public static Logger getInstance() {
+		return INSTANCE;
+	}
 
-  public void error(Throwable exception)
-  {
-    log(Level.ERROR, exception);
-  }
+	public void error(String message) {
+		log(Level.ERROR, message);
+	}
 
-  public void error(Supplier<String> message)
-  {
-    log(Level.ERROR, message);
-  }
+	public void error(Throwable exception) {
+		log(Level.ERROR, exception);
+	}
 
-  public void debug(String message)
-  {
-    log(Level.DEBUG, message);
-  }
+	public void error(Supplier<String> message) {
+		log(Level.ERROR, message);
+	}
 
-  public void debug(Supplier<String> message)
-  {
-    log(Level.DEBUG, message);
-  }
+	public void debug(String message) {
+		log(Level.DEBUG, message);
+	}
 
-  public void debug(Throwable exception)
-  {
-    log(Level.DEBUG, exception);
-  }
+	public void debug(Supplier<String> message) {
+		log(Level.DEBUG, message);
+	}
 
-  public void info(String message)
-  {
-    log(Level.INFO, message);
-  }
+	public void debug(Throwable exception) {
+		log(Level.DEBUG, exception);
+	}
 
-  public void info(Supplier<String> message)
-  {
-    log(Level.INFO, message);
-  }
+	public void info(String message) {
+		log(Level.INFO, message);
+	}
 
-  public void info(Throwable exception)
-  {
-    log(Level.INFO, exception);
-  }
+	public void info(Supplier<String> message) {
+		log(Level.INFO, message);
+	}
 
-  public void warning(String message)
-  {
-    log(Level.WARNING, message);
-  }
+	public void info(Throwable exception) {
+		log(Level.INFO, exception);
+	}
 
-  public void warning(Supplier<String> message)
-  {
-    log(Level.WARNING, message);
-  }
+	public void warning(String message) {
+		log(Level.WARNING, message);
+	}
 
-  public void warning(Exception exception)
-  {
-    log(Level.WARNING, exception);
-  }
+	public void warning(Supplier<String> message) {
+		log(Level.WARNING, message);
+	}
 
-  public void trace(String message)
-  {
-    log(Level.TRACE, message);
-  }
+	public void warning(Exception exception) {
+		log(Level.WARNING, exception);
+	}
 
-  public void trace(Supplier<String> message)
-  {
-    log(Level.TRACE, message);
-  }
+	public void trace(String message) {
+		log(Level.TRACE, message);
+	}
 
-  public void trace(Throwable exception)
-  {
-    log(Level.TRACE, exception);
-  }
+	public void trace(Supplier<String> message) {
+		log(Level.TRACE, message);
+	}
 
-  public void log(Level level, String message)
-  {
-    if (isLevelActive(level))
-    {
-      printMessage(message);
-    }
-  }
+	public void trace(Throwable exception) {
+		log(Level.TRACE, exception);
+	}
 
-  public void log(Level level, Throwable exception)
-  {
-    if (isLevelActive(level))
-    {
-      exception.printStackTrace();
+	public void log(Level level, String message) {
+		if (isLevelActive(level)) {
+			printMessage(message);
+		}
+	}
 
-      throw new RuntimeException(exception);
-    }
-  }
+	public void log(Level level, Throwable exception) {
+		if (isLevelActive(level)) {
+			exception.printStackTrace();
 
-  public void log(Level level, Supplier<String> message)
-  {
-    if (isLevelActive(level))
-    {
-      printMessage(message.get());
-    }
-  }
+			throw new RuntimeException(exception);
+		}
+	}
 
-  public boolean isLevelActive(Level level)
-  {
-    return this.level.ordinal() >= level.ordinal();
-  }
+	public void log(Level level, Supplier<String> message) {
+		if (isLevelActive(level)) {
+			printMessage(message.get());
+		}
+	}
 
-  private void printMessage(String message)
-  {
-    if (className != null && methodName != null)
-    {
-      System.out.println("Class: " + className + " Method: " + methodName + methodDescriptor + " " + message);
-    }
-    else
-    {
-      System.out.println(message);
-    }
-  }
+	public boolean isLevelActive(Level level) {
+		return this.level.ordinal() >= level.ordinal();
+	}
+
+	private void printMessage(String message) {
+		if (className != null && methodName != null) {
+
+			System.out.print(startLogMessage() + "Class: " + className + //
+					" Method: " + methodName + methodDescriptor + " " + message + "\n");
+		} else {
+			System.out.print(startLogMessage() + message + "\n");
+		}
+	}
+
+	private String startLogMessage() {
+		return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " [" + level + "] ";
+	}
 
 }
