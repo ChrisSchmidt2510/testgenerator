cbd9f9021236ebdea4b449a572607bfc50dfd127
chris <chris@DESKTOP-3FU7NR4.fritz.box>
2020-01-03 19:41:56 +0100

    FileAppenderTest angepast Resource closed

--- testgenerator-core/src/test/java/de/nvg/testgenerator/logging/config/appender/FileAppenderTest.java
+++ testgenerator-core/src/test/java/de/nvg/testgenerator/logging/config/appender/FileAppenderTest.java
@@ -1,17 +1,21 @@
 package de.nvg.testgenerator.logging.config.appender;
 
+import java.io.IOException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class FileAppenderTest {
 
 	@Test
-	public void testFileAppenderFileCreation() {
+	public void testFileAppenderFileCreation() throws IOException {
 		FileAppender fileAppender = new FileAppender("Agent", 5,
 				System.getProperty("user.home") + "\\testgenerator\\agent");
 		fileAppender.write("Der Lachs löppt");
 
 		Assert.assertTrue(true);
+
+		fileAppender.close();
 	}
 
 }
