5043727746bf264109f014ce1245699d3ee7d4ed
Schmidt, Christoph <christoph.schmidt.1@gmx.de>
2020-02-08 10:45:14 +0100

    Bugfix FieldTypeChanger classNames

--- testgenerator-agent/src/main/java/de/nvg/javaagent/classdata/modification/fields/FieldTypeChanger.java
+++ testgenerator-agent/src/main/java/de/nvg/javaagent/classdata/modification/fields/FieldTypeChanger.java
@@ -33,23 +33,29 @@ import javassist.bytecode.SignatureAttribute;
 public class FieldTypeChanger {
 	private static final Logger LOGGER = LogManager.getLogger(FieldTypeChanger.class);
 
+	private static final String REFERENCE_PROXY_CLASSNAME = "de/nvg/proxy/impl/ReferenceProxy";
 	private static final String REFERENCE_PROXY = "Lde/nvg/proxy/impl/ReferenceProxy;";
 	private static final String REFERENCE_PROXY_CONSTRUCTOR = "(Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/String;)V";
 	private static final String REFERENCE_PROXY_DEFAULT_CONSTRUCTOR = "(Ljava/lang/Object;Ljava/lang/String;Ljava/lang/String;)V";
 
+	private static final String BOOLEAN_PROXY_CLASSNAME = "de/nvg/proxy/impl/BooleanProxy";
 	private static final String BOOLEAN_PROXY = "Lde/nvg/proxy/impl/BooleanProxy;";
 	private static final String BOOLEAN_PROXY_CONSTRUCTOR = "(ZLjava/lang/Object;Ljava/lang/String;)V";
 
+	private static final String DOUBLE_PROXY_CLASSNAME = "de/nvg/proxy/impl/DoubleProxy";
 	private static final String DOUBLE_PROXY = "Lde/nvg/proxy/impl/DoubleProxy;";
 	private static final String DOUBLE_PROXY_CONSTRUCTOR = "(DLjava/lang/Object;Ljava/lang/String;)V";
 
+	private static final String FLOAT_PROXY_CLASSNAME = "de/nvg/proxy/impl/FloatProxy";
 	private static final String FLOAT_PROXY = "Lde/nvg/proxy/impl/FloatProxy;";
 	private static final String FLOAT_PROXY_CONSTRUCTOR = "(FLjava/lang/Object;Ljava/lang/String;)V";
 
+	private static final String INTEGER_PROXY_CLASSNAME = "de/nvg/proxy/impl/IntegerProxy";
 	private static final String INTEGER_PROXY = "Lde/nvg/proxy/impl/IntegerProxy;";
 	private static final String INTEGER_PROXY_CONSTRUCTOR = "(ILjava/lang/Object;Ljava/lang/String;Ljava/lang/String;)V";
 	private static final String INTEGER_PROXY_DEFAULT_CONSTRUCTOR = "(Ljava/lang/Object;Ljava/lang/String;Ljava/lang/String;)V";
 
+	private static final String LONG_PROXY_CLASSNAME = "de/nvg/proxy/impl/LongProxy";
 	private static final String LONG_PROXY = "Lde/nvg/proxy/impl/LongProxy;";
 	private static final String LONG_PROXY_CONSTRUCTOR = "(LLjava/lang/Object;Ljava/lang/String;)V";
 
@@ -72,23 +78,41 @@ public class FieldTypeChanger {
 			.add(Primitives.JVM_DOUBLE, DOUBLE_PROXY)//
 			.add(Primitives.JVM_LONG, LONG_PROXY).toUnmodifiableMap();
 
+	private static final Map<String, String> PRIMITIVE_PROXIES_CLASSNAME = MapBuilder.<String, String>hashMapBuilder()//
+			.add(Primitives.JVM_BYTE, INTEGER_PROXY_CLASSNAME)//
+			.add(Primitives.JVM_BOOLEAN, BOOLEAN_PROXY_CLASSNAME)//
+			.add(Primitives.JVM_SHORT, INTEGER_PROXY_CLASSNAME)//
+			.add(Primitives.JVM_CHAR, INTEGER_PROXY_CLASSNAME)//
+			.add(Primitives.JVM_INT, INTEGER_PROXY_CLASSNAME)//
+			.add(Primitives.JVM_FLOAT, FLOAT_PROXY_CLASSNAME)//
+			.add(Primitives.JVM_DOUBLE, DOUBLE_PROXY_CLASSNAME)//
+			.add(Primitives.JVM_LONG, LONG_PROXY_CLASSNAME).toUnmodifiableMap();
+
 	private static final Map<String, String> PROXY_CONSTRUCTOR_WITH_INITALIZATION = //
 			MapBuilder.<String, String>hashMapBuilder()//
-					.add(REFERENCE_PROXY, REFERENCE_PROXY_CONSTRUCTOR)//
-					.add(BOOLEAN_PROXY, BOOLEAN_PROXY_CONSTRUCTOR)//
-					.add(INTEGER_PROXY, INTEGER_PROXY_CONSTRUCTOR)//
-					.add(FLOAT_PROXY, FLOAT_PROXY_CONSTRUCTOR)//
-					.add(DOUBLE_PROXY, DOUBLE_PROXY_CONSTRUCTOR)//
-					.add(LONG_PROXY, LONG_PROXY_CONSTRUCTOR).toUnmodifiableMap();
+					.add(REFERENCE_PROXY_CLASSNAME, REFERENCE_PROXY_CONSTRUCTOR)//
+					.add(BOOLEAN_PROXY_CLASSNAME, BOOLEAN_PROXY_CONSTRUCTOR)//
+					.add(INTEGER_PROXY_CLASSNAME, INTEGER_PROXY_CONSTRUCTOR)//
+					.add(FLOAT_PROXY_CLASSNAME, FLOAT_PROXY_CONSTRUCTOR)//
+					.add(DOUBLE_PROXY_CLASSNAME, DOUBLE_PROXY_CONSTRUCTOR)//
+					.add(LONG_PROXY_CLASSNAME, LONG_PROXY_CONSTRUCTOR).toUnmodifiableMap();
 
 	private static final Map<String, String> PROXY_SET_VALUE_DESCRIPTOR = //
 			MapBuilder.<String, String>hashMapBuilder()//
-					.add(REFERENCE_PROXY, JVMTypes.OBJECT) //
-					.add(INTEGER_PROXY, Primitives.JVM_INT)//
-					.add(BOOLEAN_PROXY, Primitives.JVM_BOOLEAN)//
-					.add(FLOAT_PROXY, Primitives.JVM_FLOAT)//
-					.add(DOUBLE_PROXY, Primitives.JVM_DOUBLE)//
-					.add(LONG_PROXY, Primitives.JVM_LONG).toUnmodifiableMap();
+					.add(REFERENCE_PROXY_CLASSNAME, JVMTypes.OBJECT) //
+					.add(INTEGER_PROXY_CLASSNAME, Primitives.JVM_INT)//
+					.add(BOOLEAN_PROXY_CLASSNAME, Primitives.JVM_BOOLEAN)//
+					.add(FLOAT_PROXY_CLASSNAME, Primitives.JVM_FLOAT)//
+					.add(DOUBLE_PROXY_CLASSNAME, Primitives.JVM_DOUBLE)//
+					.add(LONG_PROXY_CLASSNAME, Primitives.JVM_LONG).toUnmodifiableMap();
+
+	private static final Map<String, String> PROXY_FIELD_MAPPER = MapBuilder.<String, String>hashMapBuilder()
+			.add(REFERENCE_PROXY_CLASSNAME, REFERENCE_PROXY)//
+			.add(INTEGER_PROXY_CLASSNAME, INTEGER_PROXY)//
+			.add(BOOLEAN_PROXY_CLASSNAME, BOOLEAN_PROXY)//
+			.add(FLOAT_PROXY_CLASSNAME, FLOAT_PROXY)//
+			.add(DOUBLE_PROXY_CLASSNAME, DOUBLE_PROXY)//
+			.add(LONG_PROXY_CLASSNAME, LONG_PROXY).toUnmodifiableMap();
 
 	private final List<FieldData> fields;
 	private final ConstPool constantPool;
@@ -122,7 +146,7 @@ public class FieldTypeChanger {
 			int putFieldIndex = instructions.indexOf(instruction);
 			Instruction instructionBeforePutField = instructions.get(putFieldIndex - 1);
 
-			String proxy = getProxy(instruction.getType());
+			String proxy = getProxyClassname(instruction.getType());
 
 			Bytecode beforeValueCreation = new Bytecode(constantPool);
 			beforeValueCreation.addNew(proxy);
@@ -137,7 +161,7 @@ public class FieldTypeChanger {
 			afterValueCreation.addAload(0);
 			afterValueCreation.addLdc(instruction.getName());
 
-			if (INTEGER_PROXY.equals(proxy)) {
+			if (INTEGER_PROXY_CLASSNAME.equals(proxy)) {
 				afterValueCreation.addLdc(Descriptor.toClassName(instruction.getType()));
 			}
 
@@ -145,7 +169,7 @@ public class FieldTypeChanger {
 				afterValueCreation.addLdc(Descriptor.toClassName(instruction.getType()));
 
 				afterValueCreation.addInvokespecial(proxy, MethodInfo.nameInit, REFERENCE_PROXY_DEFAULT_CONSTRUCTOR);
-				afterValueCreation.addPutfield(loadingClass, instruction.getName(), proxy);
+				afterValueCreation.addPutfield(loadingClass, instruction.getName(), PROXY_FIELD_MAPPER.get(proxy));
 
 				// aconst_null (1) + putField(3)
 				iterator.insertGapAt(instructionBeforePutField.getCodeArrayIndex() + codeArrayIndexModificator,
@@ -159,7 +183,7 @@ public class FieldTypeChanger {
 
 				afterValueCreation.addInvokespecial(proxy, MethodInfo.nameInit,
 						PROXY_CONSTRUCTOR_WITH_INITALIZATION.get(proxy));
-				afterValueCreation.addPutfield(loadingClass, instruction.getName(), proxy);
+				afterValueCreation.addPutfield(loadingClass, instruction.getName(), PROXY_FIELD_MAPPER.get(proxy));
 
 				iterator.insertGapAt(instruction.getCodeArrayIndex() + codeArrayIndexModificator,
 						afterValueCreation.getSize() - 3, true);
@@ -178,15 +202,11 @@ public class FieldTypeChanger {
 					stream -> Instructions.showCodeArray(stream, iterator, constantPool));
 		}
 
-		for (
-
-		FieldData field :
-
-		getUnitializedFields(initalizedFields)) {
+		for (FieldData field : getUnitializedFields(initalizedFields)) {
 
 			String dataType = Descriptor.of(field.getDataType());
 
-			String proxy = getProxy(dataType);
+			String proxy = getProxyClassname(dataType);
 
 			Bytecode bytecode = new Bytecode(constantPool);
 			bytecode.addAload(0);
@@ -195,12 +215,12 @@ public class FieldTypeChanger {
 			bytecode.addAload(0);
 			bytecode.addLdc(field.getName());
 
-			if (REFERENCE_PROXY.equals(proxy) || INTEGER_PROXY.equals(proxy)) {
+			if (REFERENCE_PROXY_CLASSNAME.equals(proxy) || INTEGER_PROXY_CLASSNAME.equals(proxy)) {
 				bytecode.addLdc(Descriptor.toClassName(dataType));
 			}
 
 			bytecode.addInvokespecial(proxy, MethodInfo.nameInit, getInitDescriptor(proxy));
-			bytecode.addPutfield(loadingClass, field.getName(), proxy);
+			bytecode.addPutfield(loadingClass, field.getName(), PROXY_FIELD_MAPPER.get(proxy));
 
 			if (codeArrayLastPutFieldInstruction == 0) {
 				codeArrayLastPutFieldInstruction = 4;
@@ -270,9 +290,9 @@ public class FieldTypeChanger {
 						+ codeArrayModificator.getModificator(loadInstruction.getCodeArrayIndex());
 
 				String dataType = instruction.getType();
-				String proxy = getProxy(dataType);
+				String proxy = getProxyClassname(dataType);
 				Bytecode beforeLoad = new Bytecode(constantPool);
-				beforeLoad.addGetfield(loadingClass, instruction.getName(), proxy);
+				beforeLoad.addGetfield(loadingClass, instruction.getName(), PROXY_FIELD_MAPPER.get(proxy));
 
 				if (loadInstruction.getCodeArrayIndex() == 0) {
 					iterator.insertAt(1, beforeLoad.get());
@@ -297,17 +317,17 @@ public class FieldTypeChanger {
 			for (Instruction instruction : getFieldInstructions) {
 
 				String dataType = instruction.getType();
-				String proxy = getProxy(dataType);
+				String proxy = getProxyClassname(dataType);
 
 				int codeArrayIndex = instruction.getCodeArrayIndex()
 						+ codeArrayModificator.getModificator(instruction.getCodeArrayIndex());
 
 				Bytecode bytecode = new Bytecode(constantPool);
-				bytecode.addGetfield(instruction.getClassRef(), instruction.getName(), proxy);
+				bytecode.addGetfield(instruction.getClassRef(), instruction.getName(), PROXY_FIELD_MAPPER.get(proxy));
 				bytecode.addInvokevirtual(proxy, getValueMethodName(dataType), getGetValueDescriptor(dataType));
 
-				if (REFERENCE_PROXY.equals(proxy)) {
-					bytecode.addCheckcast(dataType);
+				if (REFERENCE_PROXY_CLASSNAME.equals(proxy)) {
+					bytecode.addCheckcast(dataType.substring(1, dataType.length() - 1));
 					iterator.insertGapAt(codeArrayIndex, 6, true);
 					iterator.write(bytecode.get(), codeArrayIndex);
 					codeArrayModificator.addCodeArrayModificator(instruction.getCodeArrayIndex(), 6);
@@ -334,6 +354,13 @@ public class FieldTypeChanger {
 		return REFERENCE_PROXY;
 	}
 
+	private static String getProxyClassname(String dataType) {
+		if (PRIMITIVE_PROXIES_CLASSNAME.containsKey(dataType)) {
+			return PRIMITIVE_PROXIES_CLASSNAME.get(dataType);
+		}
+		return REFERENCE_PROXY_CLASSNAME;
+	}
+
 	private static String getValueMethodName(String dataType) {
 		switch (dataType) {
 		case Primitives.JVM_BYTE:
@@ -348,9 +375,9 @@ public class FieldTypeChanger {
 	}
 
 	private static String getInitDescriptor(String proxy) {
-		if (INTEGER_PROXY.equals(proxy)) {
+		if (INTEGER_PROXY_CLASSNAME.equals(proxy)) {
 			return INTEGER_PROXY_DEFAULT_CONSTRUCTOR;
-		} else if (REFERENCE_PROXY.equals(proxy)) {
+		} else if (REFERENCE_PROXY_CLASSNAME.equals(proxy)) {
 			return REFERENCE_PROXY_DEFAULT_CONSTRUCTOR;
 		}
 		return DEFAULT_PROXY_CONSTRUCTOR;
