9768f9fbcb5f22b3c7d060799de22e6f9f0771b5
Schmidt, Christoph <christoph.schmidt.1@gmx.de>
2020-02-22 19:13:59 +0100

    Load AgentProperties into RuntimeProperties

--- testgenerator-core/src/main/java/de/nvg/testgenerator/properties/RuntimeProperties.java
+++ testgenerator-core/src/main/java/de/nvg/testgenerator/properties/RuntimeProperties.java
@@ -1,7 +1,15 @@
 package de.nvg.testgenerator.properties;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RuntimeProperties {
+	private static final String JAVA_AGENT = "-javaagent";
 	private static final RuntimeProperties INSTANCE = new RuntimeProperties();
+
+	private final Map<String, String> agentProperties = new HashMap<>();
 
 	private boolean activateTracking = false;
 
@@ -20,4 +28,20 @@ public class RuntimeProperties {
 		this.activateTracking = activateTracking;
 	}
 
+	public boolean wasFieldTrackingActivated() {
+		if (agentProperties.isEmpty()) {
+			initAgentProperties();
+		}
+
+		return Boolean.valueOf(agentProperties.get(PropertyParser.ARG_TRACE_GETTER_CALLS));
+	}
+
+	private void initAgentProperties() {
+		RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
+		String javaAgent = runtime.getInputArguments().stream().filter(args -> args.startsWith(JAVA_AGENT)).findAny()
+				.orElse(null);
+		String agentArgs = javaAgent.substring(javaAgent.indexOf("=") + 1);
+
+		agentProperties.putAll(PropertyParser.parseAgentProperties(agentArgs));
+	}
 }
