2f04e1ced01ff3c68953aa96a292f8686a8aab8c
Schmidt, Christoph <christoph.schmidt.1@gmx.de>
2020-03-17 13:33:43 +0100

    Ignore FileAppenderTest temporarily

--- testgenerator-core/src/test/java/de/nvg/testgenerator/logging/config/appender/FileAppenderTest.java
+++ testgenerator-core/src/test/java/de/nvg/testgenerator/logging/config/appender/FileAppenderTest.java
@@ -3,19 +3,20 @@ package de.nvg.testgenerator.logging.config.appender;
 import java.io.IOException;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class FileAppenderTest {
 
 	@Test
+	@Ignore
 	public void testFileAppenderFileCreation() throws IOException {
-		FileAppender fileAppender = new FileAppender("Agent", 5,
-				System.getProperty("user.home") + "\\testgenerator\\agent");
-		fileAppender.write("Der Lachs löppt");
+		try (FileAppender fileAppender = new FileAppender("Agent", 5,
+				System.getProperty("user.home") + "\\testgenerator\\agent")) {
+			fileAppender.write("Der Lachs löppt");
+		}
 
 		Assert.assertTrue(true);
-
-		fileAppender.close();
 	}
 
 }
