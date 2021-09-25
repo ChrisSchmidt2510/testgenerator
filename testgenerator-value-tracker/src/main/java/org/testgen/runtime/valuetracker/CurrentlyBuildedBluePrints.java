d0519d6000318c6d1da9404fe7390a6bf0a0fa8f
Schmidt, Christoph <christoph.schmidt.1@gmx.de>
2021-09-25 15:50:35 +0200

    implement Tests for Class CurrentlyBuildedBluePrints + small Bugfixes

--- testgenerator-value-tracker/src/main/java/org/testgen/runtime/valuetracker/CurrentlyBuildedBluePrints.java
+++ testgenerator-value-tracker/src/main/java/org/testgen/runtime/valuetracker/CurrentlyBuildedBluePrints.java
@@ -16,81 +16,79 @@ public class CurrentlyBuildedBluePrints {
 	void register(Object value) {
 		register.put(value, new ArrayList<>());
 	}
-	
+
 	public boolean isCurrentlyBuilded(Object value) {
 		return register.containsKey(value);
 	}
-	
+
 	public void addFinishedListener(Object value, Consumer<BluePrint> action) {
 		if (register.containsKey(value))
 			register.get(value).add(action);
-
-		throw new IllegalArgumentException("BluePrint isnt currently builded");
+		else
+			throw new IllegalArgumentException("BluePrint isnt currently builded");
 
 	}
-	
+
 	public void addFinishedListener(Object key, Object value, BiConsumer<BluePrint, BluePrint> action) {
 		MapEntry mapEntry = new MapEntry(key, value, action);
 		mapEntryRegister.add(mapEntry);
 	}
-	
+
 	void executeActions(Object value, BluePrint bluePrint) {
 		List<Consumer<BluePrint>> actions = register.get(value);
-		
-		if(actions != null) {
+
+		if (actions != null) {
 			for (Consumer<BluePrint> action : actions) {
 				action.accept(bluePrint);
 			}
 		}
-		
+
 		register.remove(value);
-		
+
 		for (MapEntry mapEntry : mapEntryRegister) {
-			if(mapEntry.key == value) {
+			if (mapEntry.key == value) {
 				mapEntry.keyBluePrint = bluePrint;
 			}
-			
-			if(mapEntry.value == value) {
+
+			if (mapEntry.value == value) {
 				mapEntry.valueBluePrint = bluePrint;
 			}
 		}
-		
+
 		for (int i = 0; i < mapEntryRegister.size(); i++) {
 			MapEntry mapEntry = mapEntryRegister.get(i);
-			
-			if(mapEntry.canActionBeExecuted()) {
+
+			if (mapEntry.canActionBeExecuted()) {
 				mapEntry.action();
+				
+				mapEntryRegister.remove(mapEntry);
 			}
-			
-			mapEntryRegister.remove(mapEntry);
-			
+
 		}
-		
-		
+
 	}
-	
-	class MapEntry{
+
+	class MapEntry {
 		final Object key;
 		final Object value;
 		final BiConsumer<BluePrint, BluePrint> action;
-		
+
 		BluePrint keyBluePrint;
 		BluePrint valueBluePrint;
-		
-		MapEntry(Object key, Object value, BiConsumer<BluePrint, BluePrint> action){
+
+		MapEntry(Object key, Object value, BiConsumer<BluePrint, BluePrint> action) {
 			this.key = key;
 			this.value = value;
 			this.action = action;
 		}
-		
+
 		boolean canActionBeExecuted() {
 			return keyBluePrint != null && valueBluePrint != null;
 		}
-		
+
 		void action() {
 			action.accept(keyBluePrint, valueBluePrint);
 		}
-		
-		
+
 	}
 }
